92c0243e706ad9236ac43201d42c5cb42f061958
Anikkhan-web <anikkhan9501@gmail.com>
2021-10-04 06:55:54 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
+
+import { BrowserRouter,Switch,Route} from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import About from './components/About/About';
+import FreeClass from './components/FreeClass/FreeClass';
+import NotFound from './components/NotFound/NotFound';
+import Registration from './components/Registration/Registration';
+import Home from './components/Home/Home';
+import CourcePlan from './components/CourcePlan/CourcePlan';
+
+
+
+
 
 function App() {
+
+
   return (
+
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +33,74 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+     
+  
+<BrowserRouter>
+
+
+
+<Header></Header>
+
+
+<Switch>
+  
+  <Route exact path='/home'>
+    <Home></Home>
+
+  </Route>
+   <Route exact path='/'>
+    <Home></Home>
+
+  </Route>
+
+  <Route exact path='/about' >
+    <About></About>
+
+    </Route>
+    
+
+    <Route exact path='/course-plan'>
+      <CourcePlan></CourcePlan>
+    
+    </Route>
+
+
+    <Route exact path='/registration'>
+      <Registration></Registration>
+    
+    </Route>
+    
+    <Route exact path='/freeclass'>
+      <FreeClass></FreeClass>
+
+    
+    </Route>
+
+    <Route exact path='*'>
+      <NotFound></NotFound>
+    
+    </Route>
+    
+
+ 
+
+   
+   
+   
+
+ 
+    
+
+    
+</Switch>
+
+<Footer></Footer>
+
+
+</BrowserRouter>
+
+      
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import CourcePlan from '../CourcePlan/CourcePlan';
+import FreeClass from '../FreeClass/FreeClass';
+import Registration from '../Registration/Registration';
+
+
+const Home = () => {
+  return (
+    <div>
+
+<FreeClass></FreeClass>
+      
+  <CourcePlan></CourcePlan>
+  <Registration></Registration>
+
+      
+    </div>
+  );
+};
+
+export default Home;
+
+       
+
+            
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
